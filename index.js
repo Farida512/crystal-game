43d2f4f054f2a1eae0e2fac0269b440aa7e43931
farida <farida.agayeva20@gmail.com>
2023-02-22 23:16:10 +0400

    first

---
+++ index.js
@@ -0,0 +1,33 @@
+const target = Math.floor(Math.random() * (100-50)+50);
+let score = 0;
+let  wins = 0;
+let loss = 0;
+document.querySelector('#hedef').innerHTML = target ; 
+
+function again(){
+    score = 0;
+    $('.score').text(score);
+}
+$('.A').on('click',function() {
+    const deyer = parseInt(this.dataset.deyer);
+    score += deyer ; 
+    if(score === target){
+        alert('Qalib olduq');
+        wins ++;
+        $('#wins').text(wins);
+        again();
+    }else if ( score > target){
+        alert('uduzduq');
+        loss ++;
+        $('#loss').text(loss);
+        again();
+    }else {
+        alert("yeni deyer : " + score);
+    }
+});
+// win count
+// loss count
+// crystal selection 
+// your score 
+// target score 
+// crystal collector 
